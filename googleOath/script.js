ba2e47bc09da67896f99d8212bb35bac45dd9908
Aamuktha <aa>
2024-11-16 19:48:42 +0530

    added files

---
+++ googleOath/script.js
@@ -0,0 +1,8 @@
+const clientId="928143216893-dsmtppp1sp74o7etjgl9vu6uhcmsit16.apps.googleusercontent.com"
+function onSignIn(googleUser) {
+  var profile = googleUser.getBasicProfile();
+  console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+  console.log('Name: ' + profile.getName());
+  console.log('Image URL: ' + profile.getImageUrl());
+  console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+}
